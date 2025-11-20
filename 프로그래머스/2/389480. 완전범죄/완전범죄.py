9169369c82b5563eabc3f73e4914d959ee58bfb8
d0ngjini <dj8793@gmail.com>
2025-11-21 00:01:37 +0900

    [level 2] Title: 완전범죄, Time: 79.91 ms, Memory: 12.3 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.py
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.py
@@ -1,34 +1,28 @@
 def solution(info, n, m):
-    # 보자마자 무슨 의도의 문제인지 알아야 함
-    
-    # 모든 경우의 수 체크?
-    # 이중 포인터 같긴한데
-    # 항상 b를 먼저 진행시키고 a를 체크 -> 그리디 접근법 실패
-    
-    # dp
+    answer = 0
     dp = {}
-    dp[(0, 0, 0)] = 0
+    dp[(0,0,0)] = 0
     
     for i in range(len(info)):
         a_trace, b_trace = info[i]
         new_dp = {}
         
-        for (idx, a, b), a_total in dp.items():
+        for (idx, a, b), result in dp.items():
             if idx != i:
                 continue
-            
+                
             if a + a_trace < n:
-                new_key = (i + 1, a + a_trace, b)
-                new_total = a + a_trace
-                new_dp[new_key] = min(new_dp.get(new_key, float('inf')), new_total)
+                key = (i + 1, a + a_trace, b)
+                val = a + a_trace
+                new_dp[key] = min(new_dp.get(key, float('inf')), val)
                 
             if b + b_trace < m:
-                new_key = (i + 1, a, b + b_trace)
-                new_total = a
-                new_dp[new_key] = min(new_dp.get(new_key, float('inf')), new_total)
-        
+                key = (i + 1, a, b + b_trace)
+                val = a
+                new_dp[key] = min(new_dp.get(key, float('inf')), val)
+                
+                
         dp.update(new_dp)
-        
-        
-    answer = min((v for (i, a, b), v in dp.items() if i == len(info)), default=float('inf'))
+    
+    answer = min((v for (idx, a, b), v in dp.items() if idx == len(info)), default=float('inf'))
     return answer if answer != float('inf') else -1
